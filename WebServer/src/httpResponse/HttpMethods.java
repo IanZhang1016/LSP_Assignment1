282aed470aa458c93fef05564ecf041327c6592b
IanZhang1016 <zyy15301169@gmail.com>
2017-10-10 11:13:25 +0800

    Ian_Zhang1

---
+++ WebServer/src/httpResponse/HttpMethods.java
@@ -0,0 +1,7 @@
+package httpResponse;
+
+public interface HttpMethods{
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String HEAD = "HEAD";
+}
